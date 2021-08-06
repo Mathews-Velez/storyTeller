946ee66f551f7e342402d012345ddf7e5cac46d1
ev3ryday7ask3r <velezmathew@gmail.com>
2021-08-06 00:15:18 -0400

    first uplaod of project files

---
+++ Job.cs
@@ -0,0 +1,54 @@
+using System.Threading;
+using System.IO;
+using System;
+namespace storyTeller
+{
+    public class Job
+    {
+        //add multiple empty lines
+        public static void addEmptyLines(int amount=100){
+            for (int i = 0; i < amount; i++){
+                Console.WriteLine();
+            }
+
+        }
+        //read each line from a file
+        public static void readFile(string fileName, int delay = 40){
+            using (StreamReader reader = new StreamReader(fileName)){
+                string line;
+                while((line = reader.ReadLine()) != null){
+                    
+
+                    //for char loop in string line
+                    foreach (char c in line){
+                        Console.Write(c);
+                        System.Threading.Thread.Sleep(delay);
+                    }
+
+                    //delay between each line
+                    int miliseconds = 500;
+                    Thread.Sleep(miliseconds);
+                    //Console.WriteLine();
+                    //Console.WriteLine(delay);
+                    int speed = Console.ReadKey(true).KeyChar;
+
+                    if (speed=='i'){
+                        delay = delay - 20;
+                        addEmptyLines(2);
+                        Console.WriteLine("Speed of text print has been set to {0}",delay);
+                    } else if(speed == 'r'){
+                        delay = delay +20;
+                        addEmptyLines(2);
+                        Console.WriteLine("Speed of text print has been set to {0}",delay);
+
+                    } else {
+                    }
+                    addEmptyLines(1);
+
+
+                    //got to third base with c# today -Sasha ^
+                }
+            }
+        }    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Figgle;
+
+namespace storyTeller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // welcome user to the program
+            Console.WriteLine(FiggleFonts.Standard.Render("\n\n\nStory Teller Companion : 3\n\n\n"));
+            Job.addEmptyLines(4);
+
+            //fetch the folder with all the stories inside of it
+            string[] dirs = Directory.GetFiles("stories");
+
+            //ask user to pick from the stories available in the stories folder
+            int fileChoice;
+            Char usersChoice;
+
+            Console.WriteLine("Please select from the following stories available");
+
+            //list the current contents of the stories folder
+            int spot = 0;
+
+            foreach(string dir in dirs){
+                string fileName= Path.GetFileName(dir);
+                Console.WriteLine(spot+". "+fileName);
+                spot++;
+            }
+
+            //ask user to select which file is theirs
+                Job.addEmptyLines(3);
+                fileChoice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(dirs[fileChoice]+" has been selected\n\n\n");
+
+                Job.readFile(dirs[fileChoice], 70);
+
+
+            
+
+        }
+    }
+}
