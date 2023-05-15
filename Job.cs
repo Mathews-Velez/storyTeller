f97fe184a121cca186d5f41e67aa1c0a2748e878
mathew <velezmathew@gmail.com>
2023-05-15 14:58:22 -0400

    added image rendering for dagon story

--- Job.cs
+++ Job.cs
@@ -6,47 +6,84 @@ namespace storyTeller
     public class Job
     {
         //add multiple empty lines
-        public static void addEmptyLines(int amount=100){
-            for (int i = 0; i < amount; i++){
+        public static void addEmptyLines(int amount = 100)
+        {
+            for (int i = 0; i < amount; i++)
+            {
                 Console.WriteLine();
             }
 
         }
         //read each line from a file
-        public static void readFile(string fileName, int delay = 40){
-            using (StreamReader reader = new StreamReader(fileName)){
+        public static void readFile(string fileName, int delay = 40)
+        {
+            using (StreamReader reader = new StreamReader(fileName))
+            {
                 string line;
-                while((line = reader.ReadLine()) != null){
-                    
-
+                int lineCounter = 0;
+                while ((line = reader.ReadLine()) != null)
+                {
                     //for char loop in string line
-                    foreach (char c in line){
+                    foreach (char c in line)
+                    {
                         Console.Write(c);
                         System.Threading.Thread.Sleep(delay);
                     }
-
-                    //delay between each line
-                    //int miliseconds = 500;
-                    //Thread.Sleep(miliseconds);
+                    //depending at the line we are at, print the corresponding image depending on the story 
+                    generateImage(fileName, lineCounter);
+                    lineCounter++;
                     int speed = Console.ReadKey(true).KeyChar;
-
-                    if (speed=='i'){
+                    if (speed == 'i')
+                    {
                         delay = delay - 20;
                         addEmptyLines(2);
-                        Console.WriteLine("Speed of text print has been set to {0}",delay);
-                    } else if(speed == 'r'){
-                        delay = delay +20;
+                        Console.WriteLine("Speed of text print has been set to {0}", delay);
+                    }
+                    else if (speed == 'r')
+                    {
+                        delay = delay + 20;
                         addEmptyLines(2);
-                        Console.WriteLine("Speed of text print has been set to {0}",delay);
+                        Console.WriteLine("Speed of text print has been set to {0}", delay);
 
-                    } else {
                     }
                     addEmptyLines(1);
-
 
                     //got to third base with c# today -Sasha ^
                 }
             }
-        }    
+        }
+        /// <summary>
+        /// This function generates an image with a specified filename and line number.
+        /// </summary>
+        /// <param name="filename">The filename parameter is a string that represents the name of the
+        /// image file that will be generated.</param>
+        /// <param name="line">The line parameter is an integer that represents the line number in the
+        /// story where the image will be generated for.</param>
+        public static void generateImage(string filename, int line)
+        {
+            addEmptyLines(1);
+            //fetch all the image txt files and store them in an array 
+            string[] images = Directory.GetFiles("images");
+            //note: line 12 for dagon to render the monolith 1 index
+            //note: line 15 for dagon to render the creature 2 inddex
+            //note: line 4  for dagon to render the landscape of black slime
+            if (line == 12)
+            {
+                string[] display = File.ReadAllLines(images[1]);
+                foreach (string row in display)
+                {
+                    Console.WriteLine(row);
+                }
+            }
+            else if (line == 15)
+            {
+                string[] display = File.ReadAllLines(images[2]);
+                foreach (string row in display)
+                {
+                    Console.WriteLine(row);
+                }
+            }
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,54 +8,63 @@ namespace storyTeller
     {
         static void Main(string[] args)
         {
-            Boolean loop=true;
+            Boolean loop = true;
 
             // welcome user to the program
-            while (loop){
+            while (loop)
+            {
                 Job.addEmptyLines(100);
-            string mssg = FiggleFonts.Standard.Render("\n\n\nStory Teller Companion\n\n\n");
-            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}",mssg));
-            Job.addEmptyLines(4);
+                string mssg = FiggleFonts.Standard.Render("\n\n\nLovecraft's interactive story teller\n\n\n");
+                Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", mssg));
+                Job.addEmptyLines(4);
 
-            //fetch the folder with all the stories inside of it
-            string[] dirs = Directory.GetFiles("stories");
+                //fetch the folder with all the stories inside of it
+                string[] dirs = Directory.GetFiles("stories");
 
-            //ask user to pick from the stories available in the stories folder
-            int fileChoice;
-            
+                Console.WriteLine("Please select from the following stories available");
 
-            Console.WriteLine("Please select from the following stories available");
+                foreach (string dir in dirs)
+                {
+                    string fileName = Path.GetFileName(dir);
+                    Console.WriteLine(Array.IndexOf(dirs, dir) + ". " + fileName);
+                }
 
-            //list the current contents of the stories folder
-            int spot = 0;
+                //ask user to select which file is theirs
+                Job.addEmptyLines(3);
+                int fileChoice = 0;
+                bool valid = true;
+                //validate user input
+                while (valid)
+                {
+                    try
+                    {
+                        fileChoice = Convert.ToInt32(Console.ReadLine());
+                        valid = false;
+                    }
+                    catch (System.Exception)
+                    {
+                        Console.WriteLine("Please enter a valid choice.");
+                    }
+                }
+                Job.addEmptyLines(3);
+                String nameOfFile = Path.GetFileNameWithoutExtension(dirs[fileChoice]);
+                Console.WriteLine(nameOfFile + " has been selected\n\n\n");
 
-            foreach(string dir in dirs){
-                string fileName= Path.GetFileName(dir);
-                Console.WriteLine(spot+". "+fileName);
-                spot++;
-            }
+                //tell the user how to use use the readfile method
+                Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "Instructions"));
+                Console.WriteLine("After each line is printed from the selected story you will be able to enter 'r' (reduce) or 'i' (increase) to manipulate the speed at which the text is being printed otherwise press enter to continue the story. Press any key to continue.");
+                Console.ReadKey();
+                Job.addEmptyLines(10);
+                Job.readFile(dirs[fileChoice], 10);
+                Job.addEmptyLines(5);
 
-            //ask user to select which file is theirs
-            Job.addEmptyLines(3);
-            fileChoice = Convert.ToInt32(Console.ReadLine());
-            Job.addEmptyLines(3);
-            String nameOfFile = Path.GetFileNameWithoutExtension(dirs[fileChoice]);
-            Console.WriteLine(nameOfFile+" has been selected\n\n\n");
+                Console.WriteLine("Would you like to read another story?(y or n)");
+                char usersChoice = Console.ReadLine()[0];
 
-            //tell the user how to use use the readfile method
-            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}","Instructions"));
-            Console.WriteLine("After each line is printed from the selected story you will be able to enter 'r' (reduce) or 'i' (increase) to manipulate the speed at which the text is being printed otherwise press enter to continue the story. Press any key to continue.");
-            Console.ReadKey();
-            Job.addEmptyLines(10);
-            Job.readFile(dirs[fileChoice], 70);
-            Job.addEmptyLines(5);
-
-            Console.WriteLine("Would you like to read another story?(y or n)");
-            char usersChoice = Console.ReadLine()[0];
-
-            if (usersChoice == 'n'){
-                loop = false;
-            }
+                if (usersChoice == 'n')
+                {
+                    loop = false;
+                }
 
             }
         }
